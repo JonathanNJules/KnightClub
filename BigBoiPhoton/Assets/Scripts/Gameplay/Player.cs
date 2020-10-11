6b580ac6634567060bd1230d2b7ee5bcb3f8b6f8
Jonathan <jonathannjules@gmail.com>
2020-10-10 21:41:32 -0400

    Animation
    animation now working locally and over the network

--- BigBoiPhoton/Assets/Scripts/Gameplay/Player.cs
+++ BigBoiPhoton/Assets/Scripts/Gameplay/Player.cs
@@ -1,18 +1,22 @@
 ﻿using Photon.Pun;
+using System.Reflection;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviourPunCallbacks, IPunObservable
 {
     private Rigidbody rb;
     public Transform model;
+    private Animator anim;
     public Transform camLookT;
     private Vector3 moveVector;
     public float moveSpeed;
 
     private Vector3 networkedPos;
     private Quaternion networkedRot;
+    private float networkedMovementMag;
+    private int networkedEmoteIndex;
+    private bool networkedCutEmote;
     public float networkSmoothingSpeed;
 
     public TMP_Text nameText;
@@ -25,6 +29,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         DontDestroyOnLoad(gameObject);
 
         nameText.text = photonView.Owner.NickName;
+        anim = model.GetChild(0).GetComponent<Animator>();
 
         if (!photonView.IsMine) return;
 
@@ -42,6 +47,9 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
 
     void Update()
     {
+        anim.SetFloat("MovementMag", networkedMovementMag);
+        anim.SetBool("CutEmote", networkedCutEmote);
+        anim.SetInteger("EmoteIndex", networkedEmoteIndex);
         if (photonView.IsMine) OwnerUpdate();
         else NetworkedUpdate();
     }
@@ -52,6 +60,11 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         moveVector = cmr.inChatBox ? Vector3.zero : new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         if (moveVector.sqrMagnitude > 1) moveVector = moveVector.normalized;
+        networkedMovementMag = moveVector.magnitude;
+
+        Emotes();
+        networkedCutEmote = moveVector.magnitude > 0.02f;
+
         moveVector *= moveSpeed;
     }
 
@@ -61,12 +74,28 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         model.rotation = Quaternion.Lerp(model.rotation, networkedRot, networkSmoothingSpeed * Time.deltaTime);
     }
 
+    void Emotes() {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            networkedEmoteIndex = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            networkedEmoteIndex = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            networkedEmoteIndex = 3;
+
+        if (Input.GetKeyUp(KeyCode.Alpha1) && networkedEmoteIndex == 1)
+            networkedEmoteIndex = 0;
+        if (Input.GetKeyUp(KeyCode.Alpha2) && networkedEmoteIndex == 2)
+            networkedEmoteIndex = 0;
+        if (Input.GetKeyUp(KeyCode.Alpha3) && networkedEmoteIndex == 3)
+            networkedEmoteIndex = 0;
+    }
+
     void FixedUpdate()
     {
         if (!photonView.IsMine) return;
 
         rb.velocity = moveVector * Time.deltaTime;
-        if (moveVector.sqrMagnitude > 0.5f)
+        if (moveVector.magnitude > .5f)
         {
             float ang = Mathf.Atan2(moveVector.z, moveVector.x) * Mathf.Rad2Deg - 90;
             model.eulerAngles = new Vector3(0, Mathf.LerpAngle(model.eulerAngles.y, -ang, 15 * Time.deltaTime), 0);
@@ -79,11 +108,17 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         {
             stream.SendNext(transform.position);
             stream.SendNext(model.rotation);
+            stream.SendNext(networkedMovementMag);
+            stream.SendNext(networkedEmoteIndex);
+            stream.SendNext(networkedCutEmote);
         }
         else
         {
             networkedPos = (Vector3)stream.ReceiveNext();
             networkedRot = (Quaternion)stream.ReceiveNext();
+            networkedMovementMag = (float)stream.ReceiveNext();
+            networkedEmoteIndex = (int)stream.ReceiveNext();
+            networkedCutEmote = (bool)stream.ReceiveNext();
         }
     }
 
